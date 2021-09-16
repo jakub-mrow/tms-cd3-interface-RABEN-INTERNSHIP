e8552c8894cdcf2ace0c619b221b802363f00a07
Jakub Mrowczynski <jbmi11@raben-group.com>
2021-09-16 10:51:17 +0200

    added logging

--- app/api/main.py
+++ app/api/main.py
@@ -1,10 +1,35 @@
 from flask import Flask
 from flask import jsonify, request
 from data import Data
+from datetime import date
 import contman_conn as cont
 import time
+import uuid
+import logging
+import os
 
 app = Flask(__name__)
+
+# <----------------------------------------------->
+# Disable flask internal logging
+# comment block below to turn it on, but it will also
+# log to file additionall flask logs besides custom logs
+# <----------------------------------------------->
+app.logger.disabled = True
+log = logging.getLogger('werkzeug')
+log.disabled = True
+
+# <----------------------------------------------->
+# config for logs
+# <----------------------------------------------->
+logging.basicConfig(
+    filename="app/logs/"+str(date.today()),
+    filemode="a",
+    format="%(asctime)s | %(levelname)s | %(message)s",
+    datefmt="%H:%M:%S",
+    level=logging.INFO,
+    encoding='utf-8'
+)
 
 @app.route("/")
 def hello_world():
@@ -12,6 +37,7 @@ def hello_world():
 
 @app.route("/documents", methods = ["POST"])
 def sendFile():
+    requestID = str(uuid.uuid4())
     if request.is_json:
         # <----------------------------------------------->
         # setting up data object to store repeating values
@@ -26,7 +52,8 @@ def sendFile():
         
         dataObj, loginError = cont.login(dataObj)
         if loginError is not None:
-            return loginError
+            logging.error("{} | {} | {}".format(requestID, "login", loginError["request-error"]))
+            return loginError, loginError["request-error"][0]
 
         # <----------------------------------------------->
         #  error handle: check if document class exists,
@@ -34,36 +61,59 @@ def sendFile():
         # <----------------------------------------------->
         checkDocumentClass = cont.getDocumentClassID(data["documentClass"],dataObj)
         if type(checkDocumentClass) is dict:
-            return checkDocumentClass
+            logging.error("{} | {} | {}".format(requestID, "document class", checkDocumentClass["request-error"]))
+            return checkDocumentClass, 400
 
         dataObj, startError = cont.startTransaction(dataObj)
         if startError is not None:
-            return startError
+            logging.error("{} | {} | {}".format(requestID, "start", startError["request-error"]))
+            return startError, 500
 
         dataObj, check = cont.convertFormat(data, dataObj)
         if check == False:
-            return {"error": "Invalid structure of the format. One of the indexes does not exist in this document class!"}
+            formatError = {"request-error": "Invalid structure of the format. One of the indexes does not exist in this document class!"}
+            logging.error("{} | {} | {}".format(requestID, "format", formatError["request-error"]))
+            return formatError, 400
         
         dataObj, createError = cont.createDocument(dataObj)
         if createError is not None:
-            return createError
+            logging.error("{} | {} | {}".format(requestID, "create", createError["request-error"]))
+            return createError, 500
 
         dataObj, uploadError = cont.uploadFile(dataObj)
         if uploadError is not None:
-            return uploadError
+            logging.error("{} | {} | {}".format(requestID, "upload", uploadError["request-error"]))
+            return uploadError, 500
 
         dataObj, commitError = cont.commitTransaction(dataObj)
         if commitError is not None:
-            return commitError
+            logging.error("{} | {} | {}".format(requestID, "commit", commitError["request-error"]))
+            return commitError, 500
 
         dataObj, logoutError = cont.logout(dataObj)
         if logoutError is not None:
-            return logoutError
+            logging.error("{} | {} | {}".format(requestID, "logout", logoutError["request-error"]))
+            return logoutError, 500
 
         requestTime = round(float(time.time() - start), 4)
-        return {"response": "File added succefully!", "time": requestTime}, 201
-        
-    return {"error": "Request must be JSON"}
+        goodRes = {"response": "File added succefully!", "time": requestTime}
+        logging.info("{} | {}".format(requestID, goodRes["response"]))
+        return goodRes, 201
+
+    jsonError = {"request-error": "Request must be JSON"}
+    logging.error("{} | {}".format(requestID, jsonError["request-error"]))    
+    return jsonError, 400
 
 if __name__ == "__main__":
-    app.run()
+    # <----------------------------------------------->
+    # create file for logging
+    # <----------------------------------------------->
+    dateFile = str(date.today())
+    if not(os.path.isfile("logs/"+dateFile)):
+        fileLog = open("logs/"+dateFile, "w+")
+        fileLog.close()
+
+    # <----------------------------------------------->
+    # run the service
+    # <----------------------------------------------->
+    app.run(debug=True)
